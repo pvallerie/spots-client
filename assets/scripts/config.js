65b15436edbe5d956c67d7cf40181535f5457d4c
pvallerie <petevallerie@gmail.com>
2021-01-07 14:58:31 -0500

    Remove / from config deployed url
    Removed the / after my deployed API url in my config.js file to see if
    it will fix the 404 error I'm getting.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://polar-savannah-31336.herokuapp.com/',
+  production: 'https://polar-savannah-31336.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
